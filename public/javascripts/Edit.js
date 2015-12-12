9e5a600aced4c80f658e91ab2b1300d13c65a67a
Myoung-jae-Kim <rlaaud30@naver.com>
2015-12-12 22:26:59 +0900

    select options serialize

--- public/javascripts/Edit.js
+++ public/javascripts/Edit.js
@@ -1,30 +1,33 @@
 /*global $:false */
 /*global _:false */
 /*jslint browser:true, devel: true */
+$(document).ready(function(){
   $(".btn-primary").click(function(e) {
     $(e.currentTarget).hide();
     $(".btn-success").show();
   });
 
   $(".create").click(function() {
+    console.log("click");
     if($(".survey-options").children("option:selected").text() === "객관식") {
-      $("ol").append("<li class='choice'><span><input type='text'></span><form><input type='radio' name='select' value='1'> <input type='text'><br><input type='radio' name='select' value='2'> <input type='text'><br><input type='radio' name='select' value='3'> <input type='text'><br><input type='radio' name='select' value='4'> <input type='text'><br><input type='radio' name='select' value='5'> <input type='text'><br><input type='radio' name='select' value='6'> 기타<input type='text'></form></li><br>");
+      $("ol").append("<div class='choice'><li><span><input type='text'></span><div><input type='radio' id='select' name='select' value='1'> <input type='text'><br><input type='radio' id='select' name='select' value='2'> <input type='text'><br><input type='radio' id='select' name='select' value='3'> <input type='text'><br><input type='radio' id='select' name='select' value='4'> <input type='text'><br><input type='radio' id='select' name='select' value='5'> <input type='text'><br><input type='radio' id='select' name='select' value='6'> 기타<input type='text'></div></li></div><br>");
     } else if($(".survey-options").children("option:selected").text() === "텍스트") {
-      $("ol").append("<li class='text'><span><input type='text'></input></span><div><input class='form-control' type='text'></input></div></li><br>");
+      $("ol").append("<div class='text'><li><span><input type='text'></input></span><div><input class='form-control' type='text'></input></div></li></div><br>");
     } else {
-      $("ol").append("<li class='longtext'><span><input type='text'></input></span><br><div><textarea class='form-control' rows='10' placeholder='긴의견'></textarea></div></li><br>");
+      $("ol").append("<div class='longtext'><li><span><input type='text'></input></span><br><div><textarea class='form-control' rows='10' placeholder='긴의견'></textarea></div></li></div><br>");
     }
   });
 
- $("li").each(function() {
-   if($("li").hasClass("choice")) {
-    $("li").serializeArray();
-    console.log(this);
-  } else if($("li").hasClass("text")) {
-    $("li").serializeArray();
-    console.log(this);
-  } else if($("li").hasClass("longtext")) {
-    $("li").serializeArray();
-    console.log(this);
+ $("div").each(function() {
+   if($("div").hasClass("choice")) {
+    var choice = $(".choice").serializeArray();
+    console.log(choice);
+  } else if($("div").hasClass("text")) {
+    var text = $(".text").serializeArray();
+    console.log(text);
+  } else if($("div").hasClass("longtext")) {
+    var longtext = $(".longtext").serializeArray();
+    console.log(longtext);
   }
  });
+});
